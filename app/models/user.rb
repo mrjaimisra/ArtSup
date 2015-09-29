281f0ff67b56467b73f7d607d32cf604ac4ec0cc
mrjaimisra <misrajai01@gmail.com>
2015-09-29 13:52:55 -0600

    User can add collection with test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   def self.find_or_create_from_auth_hash(data)
     user = User.find_or_create_by(provider: data.provider, uid: data.uid)
-
     user.email      = data.info.email
     user.name       = data.info.name
     user.image_url  = data.info.image
--- spec/features/user_adds_collection_spec.rb
+++ spec/features/user_adds_collection_spec.rb
@@ -1,13 +1,30 @@
 require 'rails_helper'
 
 RSpec.feature "User creates a new collection", type: :feature do
-  scenario "successfully" do
-    visit root_path
 
-    fill_in :title, with: "Film Project"
-    fill_in :description, with: "GoPro trip to the desert to film sand boarding"
+  before do
+    Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:amazon]
+  end
+
+  scenario "successfully" do
+
+    login
+
+    visit root_path
+    click_link "Login"
+
+    expect(current_path).to eq(dashboard_path)
+
+    within(".g-tabs") do
+      click_on "Collections"
+    end
+
+    expect(current_path).to eq(new_users_collection_path(1))
+
+    fill_in "Title", with: "Film Project"
+    fill_in "Description", with: "GoPro trip to the desert to film sand boarding"
     click_on "Create"
 
-    expect(current_path).to eq(users_collection_path(collection.id))
+    expect(current_path).to eq(dashboard_path)
   end
 end
