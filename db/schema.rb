acdbad0b605794e9d7c8a05ad5d611ab49e03fc4
mrjaimisra <misrajai01@gmail.com>
2015-09-30 10:35:34 -0600

    Rollback db and re run paperclip migration for collections

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150929203859) do
+ActiveRecord::Schema.define(version: 20150930163357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,13 +33,9 @@ ActiveRecord::Schema.define(version: 20150929203859) do
   create_table "pieces", force: :cascade do |t|
     t.string   "name"
     t.string   "description"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.integer  "collection_id"
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
   end
 
   add_index "pieces", ["collection_id"], name: "index_pieces_on_collection_id", using: :btree
