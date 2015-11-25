f400f7757e0d253c8c2ce031a75248bec68e3284
mrjaimisra <misrajai01@gmail.com>
2015-11-25 16:43:00 -0700

    try out two emails

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,7 +6,8 @@ class NotificationsController < ApplicationController
   def create
     @user = User.find_by(name: params[:artist])
     NotificationsMailer.contact(email_params).deliver_now
-
+    NotificationsMailer.confirm(email_params).deliver_now
+    
     ArtistToSupporter.create(artist_id: @user.id, supporter_id: current_user.id)
     redirect_to dashboard_path(current_user.url), notice: 'Email sent.'
     flash[:message] = "Email sent."
--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -14,4 +14,20 @@ class NotificationsMailer < ApplicationMailer
         subject: "Message from #{email_params[:name]}"
     )
   end
+
+  def confirm(email_params)
+    @message = email_params[:message]
+    @artist = email_params[:artist]
+    @name = email_params[:name]
+    @street_address = email_params[:street_address]
+    @city = email_params[:city]
+    @state = email_params[:state]
+    @zip_code = email_params[:zip_code]
+    @email = email_params[:email]
+
+    mail(
+        to: email_params[:email],
+        subject: "Message from #{email_params[:name]}"
+    )
+  end
 end
