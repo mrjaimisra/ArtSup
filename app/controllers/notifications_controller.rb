893d67c30c127b6dd14282ec6d7eef52c0cafa2d
mrjaimisra <misrajai01@gmail.com>
2015-11-25 16:46:37 -0700

    fix email template

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -7,7 +7,7 @@ class NotificationsController < ApplicationController
     @user = User.find_by(name: params[:artist])
     NotificationsMailer.contact(email_params).deliver_now
     NotificationsMailer.confirm(email_params).deliver_now
-    
+
     ArtistToSupporter.create(artist_id: @user.id, supporter_id: current_user.id)
     redirect_to dashboard_path(current_user.url), notice: 'Email sent.'
     flash[:message] = "Email sent."
